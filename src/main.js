cab3e6e4e3ed8223b31c3669ae4448b6c83d8d1b
lizguro <dianyguti@gmail.com>
2020-07-07 13:23:02 -0500

    main

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import { mainCharacter } from './data.js';
 
-console.log(example, data);
+import data from './data/rickandmorty/rickandmorty.js';
+
+const root = document.getElementById("root");
+const RickMortyDB = data.results;
+
+
+export const printCharacters = (mainCharacters) =>{
+    const view = mainCharacters.map(character) => {
+
+    }
+    
+
+}
+
+for (let i =0 ; i < 5 ; i++)
+{
+    root.innerHTML = RickMortyDB.name
+  
+}
