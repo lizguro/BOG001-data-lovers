f6f66450c1fb7f2f165e2fc5827666af4ea2fc5a
lizguro <dianyguti@gmail.com>
2020-07-09 18:10:37 -0500

    maincharacters

--- src/main.js
+++ src/main.js
@@ -1,21 +1,27 @@
-import { mainCharacter } from './data.js';
+//import { mainCharacter } from './data.js';
 
 import data from './data/rickandmorty/rickandmorty.js';
 
 const root = document.getElementById("root");
 const RickMortyDB = data.results;
 
+const printMainCharacters = (arrayparameter) => {
 
-export const printCharacters = (mainCharacters) =>{
-    const view = mainCharacters.map(character) => {
+    let templateCharacters = "";
 
+    for (let i = 0; i < 5; i++) {
+
+        const print = `
+            <article class="main-item">
+                <a href="#/${arrayparameter[i].id}/" class="main-conteiner">
+                    <img src="${arrayparameter[i].image}" alt="${arrayparameter[i].name}">
+                     <p>${arrayparameter[i].name}</p>
+                </a>
+            </article>`
+
+        templateCharacters = templateCharacters + print;
     }
-    
+    root.innerHTML = templateCharacters;
+};
+printMainCharacters(RickMortyDB);
 
-}
-
-for (let i =0 ; i < 5 ; i++)
-{
-    root.innerHTML = RickMortyDB.name
-  
-}
