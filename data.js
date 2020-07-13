256dedceda842015fad7a69082df67836a4c51cc
lizguro <dianyguti@gmail.com>
2020-07-13 14:20:13 -0500

    Updates

--- data.js
+++ data.js
@@ -1,10 +1,22 @@
 // estas funciones son de ejemplo
 
-export const mainCharacter = () => {
+export const orderNames = (data, option) => {
+  const orderAZ= data.sort(function(prev,next){
+    if(prev.name > next.name){
+      return 1;
+    }
+    if(prev.name < next.name){
+      return -1;
+    }
+  })
 
+  if(option == 'A-Z'){
+    return orderAZ;
+  }
+  if (option == 'Z-A'){
+    return orderAZ.reverse();
+  }
 
-
-  return 'mainCharacter';
 };
 
 export const anotherExample = () => {
